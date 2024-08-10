8a78673fffdd6effa091cf5e4bcb40727062ec29
niiko <nicolas.rocagel@gmail.com>
2024-08-10 18:21:58 +0200

    ➕ Add T3 Env

--- src/types/environment.ts
+++ src/types/environment.ts
@@ -1,46 +1,51 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { z } from 'zod'
 
-import { createEnv } from '@t3-oss/env-core'
+import { createEnv } from '@t3-oss/env-nextjs'
 
+/** This assures that the env variables are properly set up */
 export const environment = createEnv({
+    /** Server-side env variables */
     server: {
         MOCK_ENABLED: z.string(),
         BACK_ROOT: z.string(),
         USER_PORT: z.string(),
         USER_BASE_URL: z.string(),
+        ARTICLE_PORT: z.string(),
+        ARTICLE_BASE_URL: z.string(),
+        TRANSACTION_PORT: z.string(),
+        TRANSACTION_BASE_URL: z.string(),
+        INSTMESSAGE_PORT: z.string(),
+        INSTANT_MESSAGE_BASE_URL: z.string(),
     },
 
-    /**
-     * The prefix that client-side variables must have. This is enforced both at
-     * a type-level and at runtime.
-     */
-    clientPrefix: 'NEXT_PUBLIC_',
-
+    /** Client-side env variables. Starts by 'NEXT_PUBLIC_' */
     client: {
         NEXT_PUBLIC_MOCK_ENABLED: z.string(),
         NEXT_PUBLIC_BACK_ROOT: z.string(),
-        NEXT_PUBLIC_USER_BASE_URL: z.string(),
         NEXT_PUBLIC_USER_PORT: z.string(),
+        NEXT_PUBLIC_USER_BASE_URL: z.string(),
+        NEXT_PUBLIC_ARTICLE_PORT: z.string(),
+        NEXT_PUBLIC_ARTICLE_BASE_URL: z.string(),
+        NEXT_PUBLIC_TRANSACTION_PORT: z.string(),
+        NEXT_PUBLIC_TRANSACTION_BASE_URL: z.string(),
+        NEXT_PUBLIC_INSTMESSAGE_PORT: z.string(),
+        NEXT_PUBLIC_INSTANT_MESSAGE_BASE_URL: z.string(),
     },
 
-    /**
-     * What object holds the environment variables at runtime. This is usually
-     * `process.env` or `import.meta.env`.
-     */
-    runtimeEnv: process.env,
-
-    /**
-     * By default, this library will feed the environment variables directly to
-     * the Zod validator.
-     *
-     * This means that if you have an empty string for a value that is supposed
-     * to be a number (e.g. `PORT=` in a ".env" file), Zod will incorrectly flag
-     * it as a type mismatch violation. Additionally, if you have an empty string
-     * for a value that is supposed to be a string with a default value (e.g.
-     * `DOMAIN=` in an ".env" file), the default value will never be applied.
-     *
-     * In order to solve these issues, we recommend that all new projects
-     * explicitly specify this option as true.
-     */
-    emptyStringAsUndefined: true,
+    /** Also add your client-side env variables here to avoid TS errors */
+    experimental__runtimeEnv: {
+        NEXT_PUBLIC_MOCK_ENABLED: process.env.NEXT_PUBLIC_MOCK_ENABLED!,
+        NEXT_PUBLIC_BACK_ROOT: process.env.NEXT_PUBLIC_BACK_ROOT!,
+        NEXT_PUBLIC_USER_PORT: process.env.NEXT_PUBLIC_USER_PORT!,
+        NEXT_PUBLIC_USER_BASE_URL: process.env.NEXT_PUBLIC_USER_BASE_URL!,
+        NEXT_PUBLIC_ARTICLE_PORT: process.env.NEXT_PUBLIC_ARTICLE_PORT!,
+        NEXT_PUBLIC_ARTICLE_BASE_URL: process.env.NEXT_PUBLIC_ARTICLE_BASE_URL!,
+        NEXT_PUBLIC_TRANSACTION_PORT: process.env.NEXT_PUBLIC_TRANSACTION_PORT!,
+        NEXT_PUBLIC_TRANSACTION_BASE_URL:
+            process.env.NEXT_PUBLIC_TRANSACTION_BASE_URL!,
+        NEXT_PUBLIC_INSTMESSAGE_PORT: process.env.NEXT_PUBLIC_INSTMESSAGE_PORT!,
+        NEXT_PUBLIC_INSTANT_MESSAGE_BASE_URL:
+            process.env.NEXT_PUBLIC_INSTANT_MESSAGE_BASE_URL!,
+    },
 })
