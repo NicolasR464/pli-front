2c399281ca2b41f0df738561d7800a0f46c8cb83
Amanda Djouob <amandadjouob@yahoo.fr>
2024-10-04 15:56:27 +0200

    MAJ des composants

---
+++ src/mocks/components.ts
@@ -0,0 +1,28 @@
+import { Accordion, AccordionItem, AccordionTrigger, AccordionContent } from '@/components/shadcn/ui/accordion' 
+import { Checkbox } from '@/components/shadcn/ui/checkbox'
+import { 
+    navigationMenuTriggerStyle,
+    NavigationMenu,
+    NavigationMenuList,
+    NavigationMenuItem,
+    NavigationMenuContent,
+    NavigationMenuTrigger,
+    NavigationMenuLink,
+    NavigationMenuIndicator,
+    NavigationMenuViewport,
+  } from '@/components/shadcn/ui/navigation-menu'
+  import { Progress } from '@/components/shadcn/ui/progress'
+  import { RadioGroup, RadioGroupItem } from '@/components/shadcn/ui/radio-group'
+  import { ScrollArea, ScrollBar } from '@/components/shadcn/ui/scroll-area'
+  import { Separator } from '@/components/shadcn/ui/separator'
+  import { Switch } from '@/components/shadcn/ui/switch'
+  import { Tabs, TabsList, TabsTrigger, TabsContent } from '@/components/shadcn/ui/tabs'
+  import {
+    type CarouselApi,
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselPrevious,
+    CarouselNext,
+  } from '@//components/shadcn/ui/carousel'
+
