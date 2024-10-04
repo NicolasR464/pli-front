aa76707e94e61f273cbc77c2257d992b3b0ee3eb
Lorie <root@DESKTOP-TCF3JUM>
2024-10-04 19:09:43 +0200

    feat get article id from url

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -1,32 +1,13 @@
 /** Display mock product data. */
 'use client'
-// import { useSearchParams } from 'next/navigation'
-import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
-import { articles } from '../../../mocks/articles'
+import { useParams } from 'next/navigation'
 
 const Article = (): React.JSX.Element => {
-    // const SearchParams = useSearchParams()
-    const router = useRouter()
-    const [article, setArticle] = useState(null)
+    const { id } = useParams()
+    console.log('le search hereeeeee', id)
 
-    useEffect(() => {
-        const { id } = router.query
-        console.log('id from url', id)
-
-        if (id) {
-            const foundArticle = articles.find((article) => article.id.$oid)
-            setArticle(foundArticle)
-        }
-    }, [router.query])
-    // const search = SearchParams.get('id')
-    // console.log("le search hereeeeee",search)
-    // const id = router.useSearchParams(articles.id.$oid);
-    // const article = articles.find((article) => article.id.$oid == search)
-
-    if (!article) {
-        console.log('koukou', article)
-        // console.log('tout', articles);
+    if (!id) {
+        console.log('koukou', id)
         return <p>Sorry no article found</p>
     }
     return (
