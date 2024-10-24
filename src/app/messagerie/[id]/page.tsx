ee9786e67787107738a28f9a6aeccb71e3d06252
Erwan CHEREL <erwancherel.pro@gmail.com>
2024-10-24 10:54:25 +0200

    🚧 Created messagerie folder and pages; Installed socket.io-client

---
+++ src/app/messagerie/[id]/page.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { useUser } from '@clerk/nextjs'
+
+const Room = (): React.JSX.Element => {
+    // Get user info from Clerk
+    const { user } = useUser()
+
+    // Loading state while fetching the user
+    if (!user) {
+        return <div>{'Loading…'}</div>
+    }
+
+    // Component
+    return (
+        <div>
+            <h1>{'Room'}</h1>
+            <p>
+                {'Connecté : '}
+                {user.id}
+            </p>
+        </div>
+    )
+}
+
+export default Room
