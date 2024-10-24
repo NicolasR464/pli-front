05547556cbac259e8846c56ac63025c7fefa9141
niiko <nicolas.rocagel@gmail.com>
2024-10-24 21:02:35 +0200

    🎨 Finish the POST create article

--- src/utils/apiCalls/article/index.ts
+++ src/utils/apiCalls/article/index.ts
@@ -1,11 +1,16 @@
 import { articleInstance } from '@/utils/axiosInstances/article'
 import { apiEndpoints } from '@/utils/constants/endpoints'
+import { addAuthHeader } from '@/utils/functions'
 
 import type { Article } from '@/types/article'
 
 import { getUserById } from '../user'
 import type { AxiosResponse } from 'axios'
 
+/**
+ * Fetch all articles from the API.
+ * @returns {Promise<Article[]>} An array of articles.
+ */
 export const getArticles = async (): Promise<Article[]> => {
     const response: AxiosResponse<Article[]> = await articleInstance.get(
         apiEndpoints.ARTICLES,
@@ -16,6 +21,7 @@ export const getArticles = async (): Promise<Article[]> => {
 
     return response.data
 }
+
 export const getArticleById = async (id: string): Promise<Article> => {
     const response: AxiosResponse<Article> = await articleInstance.get(
         `${apiEndpoints.ARTICLES}${id}`,
@@ -37,4 +43,27 @@ export const getArticlesByUser = async (
         )
     }
     return []
+}
+/**
+ * Create a new article.
+ * @param {Partial<Article>} article - The article to create.
+ * @param {string} JWT - The JWT token for authentication.
+ * @returns {Promise<Partial<Article>>} The created article.
+ */
+export const createArticle = async (
+    article: Partial<Article>,
+    JWT: string,
+): Promise<Partial<Article>> => {
+    if (!JWT) throw new Error('No JWT provided')
+    console.log('🔥 JWT', JWT)
+
+    addAuthHeader(articleInstance, JWT)
+
+    const response: AxiosResponse<Partial<Article>> =
+        await articleInstance.post(apiEndpoints.ARTICLES, article)
+
+    if (response.status !== 201)
+        throw new Error(`Failed to create ${apiEndpoints.ARTICLES}`)
+
+    return response.data
 }
