d766d7225531a84569844618fe0ed7c0a64b8cd6
BrendaS <brenda.seck@gmail.com>
2024-10-25 11:59:17 +0200

    init components

--- src/components/shadcn/ui/skeleton.tsx
+++ src/components/shadcn/ui/skeleton.tsx
@@ -1,15 +1,15 @@
-import { cn } from '@/components/shadcn/utils'
+import { cn } from "@/components/shadcn/utils"
 
 function Skeleton({
-    className,
-    ...props
+  className,
+  ...props
 }: React.HTMLAttributes<HTMLDivElement>) {
-    return (
-        <div
-            className={cn('animate-pulse rounded-md bg-muted', className)}
-            {...props}
-        />
-    )
+  return (
+    <div
+      className={cn("animate-pulse rounded-md bg-muted", className)}
+      {...props}
+    />
+  )
 }
 
 export { Skeleton }
