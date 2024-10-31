ea74f12f2ab94a9f95a62d5db1ce1479b6cfb5f2
NSE-thik <anass.chemllal@e-thik.com>
2024-10-31 11:34:03 +0100

    add admin page

---
+++ src/admin/page.tsx
@@ -0,0 +1,9 @@
+const AdminPage = (): React.JSX.Element => {
+    return (
+        <main>
+            <h1>{'Admin User Page '}</h1>
+        </main>
+    )
+}
+
+export default AdminPage
--- src/middleware.ts
+++ src/middleware.ts
@@ -3,16 +3,36 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 // Define a matcher for protected routes
 const isProtectedRoute = createRouteMatcher(['/onboarding'])
+const isProtectedAdminRoute = createRouteMatcher(['/admin'])
+
+const adminUserIds = new Set([
+    'user_2nZYiUeq2fax63x4hiRoQNA7Xyb',
+    'user_2',
+    'user_3',
+])
 
 export default clerkMiddleware((auth, req) => {
-    if (isProtectedRoute(req)) auth().protect()
-})
+    // General protection for onboarding route
+    if (isProtectedRoute(req)) {
+        auth().protect()
+    }
+    // Protection with admin user check for admin route
+    if (isProtectedAdminRoute(req)) {
+        const { userId } = auth()
+        // Ensure the user is authenticated and is in the list of admin IDs
+        if (!userId || !adminUserIds.has(userId)) {
+            return new Response('Forbidden', { status: 403 })
+        }
+    }
 
+    return new Response('Admin', { status: 200 })
+})
 export const config = {
     matcher: [
         // Skip Next.js internals and all static files, unless found in search params
         '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
         // Always run for API routes
         '/(api|trpc)(.*)',
+        '/admin',
     ],
 }
