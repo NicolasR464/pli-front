4ce47fce1412ef6a43458b70c5b0f9ef342a3119
niiko <nicolas.rocagel@gmail.com>
2024-11-02 19:27:51 +0100

    🔨 Update getInstantMsgs call

--- src/utils/apiCalls/instantMessage/index.ts
+++ src/utils/apiCalls/instantMessage/index.ts
@@ -7,10 +7,14 @@ import type { AxiosResponse } from 'axios'
 
 export const getInstantMsgs = async (): Promise<InstantMessage[]> => {
     const response: AxiosResponse<InstantMessage[]> =
-        await instantMsgInstance.get(apiEndpoints.INSTANT_MESSAGES)
+        await instantMsgInstance.get(
+            apiEndpoints.microServices.private.INSTANT_MESSAGES,
+        )
 
     if (response.status !== 200)
-        throw new Error(`Failed to fetch ${apiEndpoints.INSTANT_MESSAGES}`)
+        throw new Error(
+            `Failed to fetch ${apiEndpoints.microServices.private.INSTANT_MESSAGES}`,
+        )
 
     return response.data
 }
