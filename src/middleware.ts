1bddc2d86ef710f4f1d2f06db1f8480ebf4cbb70
niiko <nicolas.rocagel@gmail.com>
2024-11-10 18:30:50 +0100

    🚨 Fix attempt because of CORS error

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,25 +3,45 @@ import { NextResponse } from 'next/server'
 
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-// Define a matcher for protected routes
 const isProtectedRoute = createRouteMatcher(['/onboarding'])
 
-// Create a custom middleware that combines Clerk and CORS
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type, @typescript-eslint/explicit-module-boundary-types
-const combinedMiddleware = async (req: NextRequest, event: NextFetchEvent) => {
-    // Run Clerk middleware first
+// eslint-disable-next-line unicorn/prefer-set-has
+const allowedOrigins = ['https://www.trocup.fr', 'https://trocup.fr']
+
+const combinedMiddleware = async (
+    req: NextRequest,
+    event: NextFetchEvent,
+): Promise<NextResponse | undefined> => {
+    // Handle OPTIONS request for CORS preflight
+    if (req.method === 'OPTIONS') {
+        const response = new NextResponse(undefined, { status: 200 })
+        response.headers.set('Access-Control-Allow-Origin', '*')
+        response.headers.set(
+            'Access-Control-Allow-Methods',
+            'GET, POST, PUT, DELETE, OPTIONS',
+        )
+        response.headers.set(
+            'Access-Control-Allow-Headers',
+            'Content-Type, Authorization',
+        )
+        response.headers.set('Access-Control-Max-Age', '86400')
+        return response
+    }
+
+    // Run Clerk middleware
     const clerkResponse = await clerkMiddleware((auth) => {
         if (isProtectedRoute(req)) auth().protect()
     })(req, event)
 
-    // If Clerk middleware returned a response (e.g., redirect), return it
-    if (clerkResponse) return clerkResponse
+    if (clerkResponse) return NextResponse.rewrite(clerkResponse.url)
 
-    // Otherwise, proceed with the request and add CORS headers
+    const origin = req.headers.get('origin')
     const response = NextResponse.next()
 
-    // Add CORS headers
-    response.headers.set('Access-Control-Allow-Origin', 'https://www.trocup.fr')
+    // Set CORS headers
+    if (origin && allowedOrigins.includes(origin)) {
+        response.headers.set('Access-Control-Allow-Origin', origin)
+    }
     response.headers.set(
         'Access-Control-Allow-Methods',
         'GET, POST, PUT, DELETE, OPTIONS',
@@ -39,9 +59,7 @@ export default combinedMiddleware
 
 export const config = {
     matcher: [
-        // Skip Next.js internals and all static files, unless found in search params
         String.raw`/((?!_next|[^?]*\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)`,
-        // Always run for API routes
         '/(api|trpc)(.*)',
     ],
 }
