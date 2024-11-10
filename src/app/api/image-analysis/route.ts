00bf8994d057e4926d4d8df25645d9a07b4e9a34
niiko <nicolas.rocagel@gmail.com>
2024-11-10 21:32:00 +0100

    🧪 Test api

--- src/app/api/image-analysis/route.ts
+++ src/app/api/image-analysis/route.ts
@@ -1,19 +1,36 @@
 import { NextResponse } from 'next/server'
 
+// eslint-disable-next-line jsdoc/require-jsdoc, func-style, prefer-arrow-functions/prefer-arrow-functions, @typescript-eslint/explicit-function-return-type, @typescript-eslint/require-await, @typescript-eslint/explicit-module-boundary-types
+export async function OPTIONS() {
+    return new NextResponse(undefined, {
+        status: 200,
+        headers: {
+            'Access-Control-Allow-Origin':
+                'https://trocup-front-git-front-77-nicolasr464s-projects.vercel.app',
+            'Access-Control-Allow-Methods': 'POST, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+            'Access-Control-Allow-Credentials': 'true',
+        },
+    })
+}
+
+// eslint-disable-next-line jsdoc/require-param
 /**
  * POST endpoint for storing and analyzing an image.
  * @returns {Promise<NextResponse>} A response object with the analysis result or an error message.
  */
-// eslint-disable-next-line @typescript-eslint/require-await
-export const POST = async (): Promise<NextResponse> => {
+// eslint-disable-next-line func-style, prefer-arrow-functions/prefer-arrow-functions, @typescript-eslint/require-await
+export async function POST(): Promise<NextResponse> {
     return NextResponse.json(
         { data: 'test' },
         {
+            status: 200,
             headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods':
-                    'GET, POST, PUT, DELETE, OPTIONS',
+                'Access-Control-Allow-Origin':
+                    'https://trocup-front-git-front-77-nicolasr464s-projects.vercel.app',
+                'Access-Control-Allow-Methods': 'POST, OPTIONS',
                 'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+                'Access-Control-Allow-Credentials': 'true',
             },
         },
     )
