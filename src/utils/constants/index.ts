9156a8aafbc6d446cd869122397486acdf381afb
niiko <nicolas.rocagel@gmail.com>
2024-11-10 20:34:13 +0100

    🎨 Clean up

--- src/utils/constants/index.ts
+++ src/utils/constants/index.ts
@@ -59,9 +59,3 @@ export const userMessages = {
         },
     },
 }
-
-export const corsHeaders = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-}
