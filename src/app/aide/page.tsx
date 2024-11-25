42cb28e0d952ea6c09f497cec3411e566bc6c9ca
= <amandadjouob@yahoo.fr>
2024-11-25 01:02:24 +0100

    Fix error eslint

--- src/app/aide/page.tsx
+++ src/app/aide/page.tsx
@@ -94,7 +94,7 @@ const AccordionDemo: React.FC = (): React.JSX.Element => {
                         src='/heroAide.png'
                         alt='Illustration de la plateforme Trocup'
                         layout='responsive'
-                        width={1920}  // eslint-disable-line unicorn/numeric-separators-style
+                        width={1920} // eslint-disable-line unicorn/numeric-separators-style
                         height={1000} // eslint-disable-line unicorn/numeric-separators-style
                         className='max-h-[290px] w-full object-cover shadow-lg'
                     />
